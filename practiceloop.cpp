b88af46240cdff99c96b3210c368f2c8975f29c4
tabib15-4590 <tabib15-4590@diu.edu.bd>
2023-02-03 15:38:30 +0600

    3rd

---
+++ practiceloop.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1; i<=100; i++)
+    {
+        if(i%2 == 1){
+             printf("Hello World - %d\n",i);
+        }
+
+
+    }
+
+
+    return 0;
+}
